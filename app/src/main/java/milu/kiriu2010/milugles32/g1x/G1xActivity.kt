7969fd4b8e5067a89a8f9a6bc86d2baaf86ac452
milu(Mitsuharu Kiriu) <milu.kiriu2010@gmail.com>
2019-06-24 09:25:14 +0900

    g04

--- app/src/main/java/milu/kiriu2010/milugles32/g1x/G1xActivity.kt
+++ app/src/main/java/milu/kiriu2010/milugles32/g1x/G1xActivity.kt
@@ -8,6 +8,7 @@ import milu.kiriu2010.milugles32.R
 import milu.kiriu2010.milugles32.g1x.g01.G01Fragment
 import milu.kiriu2010.milugles32.g1x.g02.G02Fragment
 import milu.kiriu2010.milugles32.g1x.g03.G03Fragment
+import milu.kiriu2010.milugles32.g1x.g04.G04Fragment
 
 class G1xActivity : AppCompatActivity() {
 
@@ -18,7 +19,7 @@ class G1xActivity : AppCompatActivity() {
         supportFragmentManager.popBackStack()
         if (supportFragmentManager.findFragmentByTag("xyz") == null) {
             supportFragmentManager.beginTransaction()
-                .replace(R.id.frameLayout, G03Fragment.newInstance(), "xyz")
+                .replace(R.id.frameLayout, G04Fragment.newInstance(), "xyz")
                 .commit()
         }
 
@@ -40,6 +41,16 @@ class G1xActivity : AppCompatActivity() {
             // 前画面に戻る
             android.R.id.home -> {
                 finish()
+                true
+            }
+            // 様々な図形
+            R.id.g04 -> {
+                supportFragmentManager.popBackStack()
+                if (supportFragmentManager.findFragmentByTag("g04") == null) {
+                    supportFragmentManager.beginTransaction()
+                        .replace(R.id.frameLayout, G04Fragment.newInstance(), "g04")
+                        .commit()
+                }
                 true
             }
             // オーブ
